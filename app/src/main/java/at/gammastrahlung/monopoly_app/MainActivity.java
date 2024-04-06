eea8960d9d01b5a0cc5090433c9608198fef1cac
majasmolej <smolej.9@gmail.com>
2024-04-06 12:37:27 +0200

    simple dice-rolling feature

--- app/src/main/java/at/gammastrahlung/monopoly_app/MainActivity.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/MainActivity.java
@@ -1,6 +1,9 @@
 package at.gammastrahlung.monopoly_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        
+        Button rollDiceButton = findViewById(R.id.rollDiceButton);
+        rollDiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to start the DiceRollingActivity
+                Intent intent = new Intent(MainActivity.this, DiceRollingActivity.class);
+                // Start the DiceRollingActivity
+                startActivity(intent);
+            }
+        });
+
     }
 }
