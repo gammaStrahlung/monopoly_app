624dad815d939c1619ff59678cef12eef0a51bf4
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-16 16:44:40 +0200

    Add databinding to GameData fields

--- app/src/main/java/at/gammastrahlung/monopoly_app/game/GameData.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/game/GameData.java
@@ -1,7 +1,10 @@
 package at.gammastrahlung.monopoly_app.game;
 
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 import androidx.databinding.ObservableArrayList;
 import androidx.databinding.ObservableInt;
+import androidx.databinding.library.baseAdapters.BR;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,9 +13,7 @@ import lombok.Setter;
 /**
  * Singleton that contains all game data
  */
-@Getter
-@Setter
-public class GameData {
+public class GameData extends BaseObservable {
     @Getter
     private static final GameData gameData = new GameData();
 
@@ -21,21 +22,43 @@ public class GameData {
     /**
      * The gameId of the current game
      */
+    @Getter
     @Setter(AccessLevel.NONE)
     private ObservableInt gameId = new ObservableInt();
 
     /**
      * Our player
      */
-    private final Player player = new Player();
+    private Player player = new Player();
+
+    @Bindable
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+        notifyPropertyChanged(BR.player);
+    }
 
     /**
      * The game and its data
      */
     private Game game;
 
+    @Bindable
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+        notifyPropertyChanged(BR.game);
+    }
+
     /**
      * List of players of the game
      */
+    @Getter
     private final ObservableArrayList<Player> players = new ObservableArrayList<>();
 }
