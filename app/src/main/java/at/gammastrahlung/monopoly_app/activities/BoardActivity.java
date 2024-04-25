8bafa9a1a4527d33c0a91f9f33e1870d7bb13e87
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-25 21:08:59 +0200

    Add field add logic to BoardActivity

--- app/src/main/java/at/gammastrahlung/monopoly_app/activities/BoardActivity.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/activities/BoardActivity.java
@@ -1,15 +1,114 @@
 package at.gammastrahlung.monopoly_app.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import at.gammastrahlung.monopoly_app.R;
+import at.gammastrahlung.monopoly_app.fragments.FieldFragment;
 
 public class BoardActivity extends AppCompatActivity {
+
+    private ConstraintLayout fieldRowTop; // Includes top corners
+    private ConstraintLayout fieldRowBottom; // Includes bottom corners
+    private ConstraintLayout fieldRowLeft;
+    private ConstraintLayout fieldRowRight;
+    private ConstraintLayout boardLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // Calls the superclass's onCreate method with the saved instance state
         setContentView(R.layout.activity_board); // Sets the content view of this activity to the activity_board_screen layout
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        fieldRowTop = findViewById(R.id.field_row_top);
+        fieldRowBottom = findViewById(R.id.field_row_bottom);
+        fieldRowLeft = findViewById(R.id.field_row_left);
+        fieldRowRight = findViewById(R.id.field_row_right);
+        boardLayout = findViewById(R.id.boardLayout);
+
+    }
+
+    private void addFieldToBoard(ConstraintLayout fieldRow, String fieldTitle, int[] playerIds, boolean showColorBar, String colorBarColor, boolean isEdge) {
+        Bundle bundle = new Bundle();
+        int colorBarPosition = FieldFragment.COLOR_BAR_NONE;
+
+        int height = 0;
+        int width = 0;
+
+        if (fieldRow == fieldRowTop || fieldRow == fieldRowBottom) {
+            height = fieldRow.getLayoutParams().height;
+
+            if (isEdge)
+                width = fieldRowLeft.getLayoutParams().width;
+        } else {
+            width = fieldRow.getLayoutParams().width;
+        }
+
+        if (showColorBar) {
+            if (fieldRow == fieldRowTop)
+                colorBarPosition = FieldFragment.COLOR_BAR_BOTTOM;
+            else if (fieldRow == fieldRowBottom)
+                colorBarPosition = FieldFragment.COLOR_BAR_TOP;
+            else if (fieldRow == fieldRowLeft)
+                colorBarPosition = FieldFragment.COLOR_BAR_RIGHT;
+            else if (fieldRow == fieldRowRight)
+                colorBarPosition = FieldFragment.COLOR_BAR_LEFT;
+        }
+
+        bundle.putInt("color_bar_position", colorBarPosition);
+        bundle.putString("color_bar_color", colorBarColor);
+        bundle.putString("title", fieldTitle);
+        bundle.putIntArray("players", playerIds);
+
+        ConstraintLayout layout = new ConstraintLayout(this);
+        fieldRow.addView(layout);
+
+        layout.getLayoutParams().height = height;
+        layout.getLayoutParams().width = width;
+        layout.setId(View.generateViewId());
+
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(layout.getId(), FieldFragment.class, bundle, "FRAG")
+                .commit();
+    }
+
+    private void addConstraints(ConstraintLayout fieldRow) {
+        // If direction is horizontal
+        boolean horizontal = fieldRow == fieldRowTop || fieldRow == fieldRowBottom;
+
+        ConstraintSet constraints = new ConstraintSet();
+        constraints.clone(fieldRow);
+
+        int[] elements = new int[fieldRow.getChildCount()];
+        for (int i = 0; i < fieldRow.getChildCount(); i++) {
+            elements[i] = fieldRow.getChildAt(i).getId();
+
+            if (horizontal) {
+                constraints.connect(fieldRow.getId(), ConstraintSet.TOP, elements[i], ConstraintSet.TOP);
+                constraints.connect(fieldRow.getId(), ConstraintSet.BOTTOM, elements[i], ConstraintSet.BOTTOM);
+            } else {
+                constraints.connect(fieldRow.getId(), ConstraintSet.START, elements[i], ConstraintSet.START);
+                constraints.connect(fieldRow.getId(), ConstraintSet.END, elements[i], ConstraintSet.END);
+            }
+        }
+
+        if (horizontal) {
+            constraints.createHorizontalChain(fieldRow.getId(), ConstraintSet.LEFT, fieldRow.getId(), ConstraintSet.RIGHT, elements, null, ConstraintSet.CHAIN_SPREAD);
+        } else {
+            constraints.createVerticalChain(fieldRow.getId(), ConstraintSet.TOP, fieldRow.getId(), ConstraintSet.BOTTOM, elements, null, ConstraintSet.CHAIN_SPREAD);
+        }
+
+        constraints.applyTo(fieldRow);
+        fieldRow.invalidate();
+        boardLayout.invalidate();
     }
 }
