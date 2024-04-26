45951a7719fa06da1160608669673159c9307e5a
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-27 01:20:36 +0200

    Add UtilityTest

---
+++ app/src/test/java/at/gammastrahlung/monopoly_app/game/UtilityTest.java
@@ -0,0 +1,45 @@
+package at.gammastrahlung.monopoly_app.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import at.gammastrahlung.monopoly_app.game.gameboard.Utility;
+
+
+public class UtilityTest {
+    Utility u;
+
+    @BeforeEach
+    void initialize() {
+        u = new Utility();
+    }
+
+    @Test
+    void owner() {
+        Player mockPlayer = Mockito.mock(Player.class);
+
+        u.setOwner(mockPlayer);
+        assertEquals(mockPlayer, u.getOwner());
+    }
+
+    @Test
+    void toPay() {
+        u.setToPay(123);
+        assertEquals(123, u.getToPay());
+    }
+
+    @Test
+    void price() {
+        u.setPrice(456);
+        assertEquals(456, u.getPrice());
+    }
+
+    @Test
+    void mortgage() {
+        u.setMortgage(789);
+        assertEquals(789, u.getMortgage());
+    }
+}
