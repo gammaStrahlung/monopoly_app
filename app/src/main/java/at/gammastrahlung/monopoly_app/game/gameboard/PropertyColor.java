0ada81ee35f28a56e793a10900719bb4a31ae894
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-26 20:14:28 +0200

    Add getColorString to PropertyColor

--- app/src/main/java/at/gammastrahlung/monopoly_app/game/gameboard/PropertyColor.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/game/gameboard/PropertyColor.java
@@ -8,5 +8,28 @@ public enum PropertyColor {
     RED,
     YELLOW,
     GREEN,
-    DARK_BLUE,
+    DARK_BLUE;
+
+    public String getColorString() {
+        switch (this) {
+            case BROWN:
+                return "#5C2117";
+            case LIGHT_BLUE:
+                return "#9EF4FF";
+            case PINK:
+                return "#FF00FF";
+            case ORANGE:
+                return "#FFAA00";
+            case RED:
+                return "#FF0000";
+            case YELLOW:
+                return "#FFFF00";
+            case GREEN:
+                return "#00AA00";
+            case DARK_BLUE:
+                return "#0000FF";
+            default:
+                return null;
+        }
+    }
 }
