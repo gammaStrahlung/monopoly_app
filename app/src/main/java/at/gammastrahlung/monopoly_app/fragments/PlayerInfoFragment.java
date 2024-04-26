e6eca5468d876cd07fc9ddd002243da9b8e9b9af
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-04-27 00:12:58 +0200

    Improve PlayerInfoFragment layout

--- app/src/main/java/at/gammastrahlung/monopoly_app/fragments/PlayerInfoFragment.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/fragments/PlayerInfoFragment.java
@@ -1,5 +1,6 @@
 package at.gammastrahlung.monopoly_app.fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.textview.MaterialTextView;
@@ -15,26 +17,27 @@ import at.gammastrahlung.monopoly_app.R;
 import at.gammastrahlung.monopoly_app.game.Player;
 
 public class PlayerInfoFragment extends DialogFragment {
-    private View inflatedView;
     private final Player player;
 
     public PlayerInfoFragment(Player player) {
         this.player = player;
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        inflatedView = inflater.inflate(R.layout.fragment_playerinfo, container, false);
-        return inflatedView;
-    }
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View inflatedView = inflater.inflate(R.layout.fragment_playerinfo, null);
+
+        // Add close button
+        builder.setView(inflatedView).setNegativeButton(R.string.close, ((dialog, which) -> dialog.cancel()));
 
         // Add actual player display code here (owned fields, balance, ...)
         MaterialTextView title = inflatedView.findViewById(R.id.player_name);
         title.setText(player.getName());
+
+        return builder.create();
     }
 }
