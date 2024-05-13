98432bb0d76f6a9fde4e99ecbc64bbb6e6855dc5
erlmatt <merlacher2001@gmail.com>
2024-05-13 09:38:33 +0200

    changed some ids in property_card.xml, referenced views in PropertyMenuActivity.java

--- app/src/main/java/at/gammastrahlung/monopoly_app/activities/PropertyMenuActivity.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/activities/PropertyMenuActivity.java
@@ -3,6 +3,8 @@ package at.gammastrahlung.monopoly_app.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +33,30 @@ public class PropertyMenuActivity extends AppCompatActivity {
             Intent intent = new Intent(PropertyMenuActivity.this, PropertyActionsMenu.class);
             // Start the PropertyActionsMenu
             startActivity(intent);
+
+            TextView propertyName = findViewById(R.id.property_name);
+            TextView currentRent = findViewById(R.id.current_rent);
+            TextView fullStackRent = findViewById(R.id.full_stack_rent);
+            TextView houseCount = findViewById(R.id.house_count);
+            TextView hotelRent = findViewById(R.id.hotel_rent);
+            TextView hotelCount = findViewById(R.id.hotel_count);
+            TextView baseRent = findViewById(R.id.base_rent);
+            TextView oneHouseRent = findViewById(R.id.one_house_rent);
+            TextView twoHouseRent = findViewById(R.id.two_house_rent);
+            TextView threeHouseRent = findViewById(R.id.three_house_rent);
+            TextView fourHouseRent = findViewById(R.id.four_house_rent);
+            TextView price = findViewById(R.id.price);
+            TextView owner = findViewById(R.id.owner);
+            ImageView buildingSlotOne = findViewById(R.id.buildingSlot1);
+            ImageView buildingSlotTwo = findViewById(R.id.buildingSlot2);
+            ImageView buildingSlotThree = findViewById(R.id.buildingSlot3);
+            ImageView buildingSlotFour = findViewById(R.id.buildingSlot4);
+
+
+
+
         });
+
+
     }
 }
