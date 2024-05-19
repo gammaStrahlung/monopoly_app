0a942eac621cac7c24ab96401039b84e4440abdd
marrabl <162188192+marrabl@users.noreply.github.com>
2024-05-19 15:36:35 +0200

    Add SelectValueFragment class

---
+++ app/src/main/java/at/gammastrahlung/monopoly_app/fragments/SelectValueFragment.java
@@ -0,0 +1,28 @@
+package at.gammastrahlung.monopoly_app.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import at.gammastrahlung.monopoly_app.R;
+
+public class SelectValueFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        View inflatedView = inflater.inflate(R.layout.fragment_valueselection, null);
+        builder.setView(inflatedView);
+
+        return builder.create();
+    }
+}
