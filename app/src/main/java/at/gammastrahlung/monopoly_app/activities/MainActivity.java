74a081fd07c9b994dc2e7472814729e7d5207262
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2024-06-09 15:09:24 +0200

    Add removal of callback once re-join checking is finished

--- app/src/main/java/at/gammastrahlung/monopoly_app/activities/MainActivity.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/activities/MainActivity.java
@@ -63,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                             preferenceEditor.remove("gameId");
                             preferenceEditor.apply();
                         }
+                        GameData.getGameData().removeOnPropertyChangedCallback(this);
                     }
                 }
             });
