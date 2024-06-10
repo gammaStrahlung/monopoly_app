8b55a0de64d1c7bd61c314b8bc18001f87b433bb
MauMauChat <lucaan@edu.aau.at>
2024-06-10 15:02:42 +0200

    Add the newInstance method to the AuctionDialogFragment class
    - Implementation of the static newInstance method that creates a new instance of AuctionDialogFragment

--- app/src/main/java/at/gammastrahlung/monopoly_app/fragments/AuctionDialogFragment.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/fragments/AuctionDialogFragment.java
@@ -3,4 +3,35 @@ package at.gammastrahlung.monopoly_app.fragments;
 import androidx.fragment.app.DialogFragment;
 
 public class AuctionDialogFragment extends DialogFragment {
+
+public static AuctionDialogFragment newInstance() {
+    return new AuctionDialogFragment();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
