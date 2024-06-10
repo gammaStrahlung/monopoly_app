6eee4f1c324a6dccebdb644cba49086e71368fff
MauMauChat <lucaan@edu.aau.at>
2024-06-10 14:28:21 +0200

    Add the newInstance method to the PurchaseDialogFragment class
    - Implementation of the static newInstance method that creates a new instance of PurchaseDialogFragment
    - Addition of propertyName and propertyId as arguments to the new PurchaseDialogFragment instance

--- app/src/main/java/at/gammastrahlung/monopoly_app/fragments/PurchaseDialogFragment.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/fragments/PurchaseDialogFragment.java
@@ -1,13 +1,33 @@
 package at.gammastrahlung.monopoly_app.fragments;
 
+import android.os.Bundle;
+
 import androidx.fragment.app.DialogFragment;
 
 public class PurchaseDialogFragment extends DialogFragment {
 
     public interface PurchaseDialogListener {
         void onNoButtonClicked();
+
         void onYesButtonClicked();
     }
 
     private PurchaseDialogListener listener;
+
+    public static PurchaseDialogFragment newInstance(String propertyName, int propertyId) {
+        PurchaseDialogFragment fragment = new PurchaseDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("property_name", propertyName);
+        args.putInt("property_id", propertyId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+
+
+
+
+
 }
