23a0bc4bce816930b639645823d79c4fcb930963
MauMauChat <lucaan@edu.aau.at>
2024-06-11 23:28:43 +0200

    "Implemented FieldObserver interface in FieldObserverImplementation class
     - Created a new class FieldObserverImplementation that implements the FieldObserver interface
     - Added an empty update method that takes a Player object as a parameter
     - This class will be used to observe changes in the field state and perform necessary actions"

---
+++ app/src/main/java/at/gammastrahlung/monopoly_app/game/FieldObserverImplementation.java
@@ -0,0 +1,13 @@
+// FieldObserverImplementation.java
+package at.gammastrahlung.monopoly_app.game;
+
+import at.gammastrahlung.monopoly_app.network.MonopolyClient;
+
+public class FieldObserverImplementation implements FieldObserver {
+    @Override
+    public void update(Player player) {
+
+    }
+
+
+}
