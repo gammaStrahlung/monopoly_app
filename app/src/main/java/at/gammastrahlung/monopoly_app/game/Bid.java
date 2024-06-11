63f1e7d883580185d704dd88e82d1874690f7b7b
MauMauChat <lucaan@edu.aau.at>
2024-06-11 15:34:05 +0200

    Refined bidding process in Bid.java by including fieldindex variable.

--- app/src/main/java/at/gammastrahlung/monopoly_app/game/Bid.java
+++ app/src/main/java/at/gammastrahlung/monopoly_app/game/Bid.java
@@ -1,5 +1,6 @@
 package at.gammastrahlung.monopoly_app.game;
 
+import at.gammastrahlung.monopoly_app.game.gameboard.Field;
 import lombok.Data;
 import java.util.UUID;
 
@@ -7,4 +8,5 @@ import java.util.UUID;
 public class Bid {
     private UUID playerId; // The ID of the player who made the bid
     private int amount; // The amount of the bid
+    private int fieldindex; // The field the bid is made on
 }
